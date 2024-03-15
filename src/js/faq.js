dc595ef6653642d189763b03e2de6d36c2864ad0
MimesisV <141736479+MimesisV@users.noreply.github.com>
2024-03-15 19:14:23 +0200

    Done

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,4 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const acc = new Accordion('.accordion-container');
+var x = new Accordion('.accordion-container', {
+    showMultiple: true,
+});
+export default x;
