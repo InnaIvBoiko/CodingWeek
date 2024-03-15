03ebdc27a304fc1a1e700fe5b352db2a171f6645
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-14 23:07:55 -0500

    main & img
    change main.js impots and add img directory

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import about from "./js/about";
+import benefits from "./js/benefits";
+import covers from "./js/covers";
+import FAQ from "./js/faq";
+import hero from "./js/hero";
+import projects from "./js/projects";
+import header from "./js/header";
+import footer from "./js/footer";
+import reviews from "./js/reviews";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import 'swiper/css';
