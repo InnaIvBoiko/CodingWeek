95803da60c37211b34d996054bc32ecf517feff2
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-16 18:45:05 -0500

    desctop version

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,2 +1,20 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+
+// const swiper = document.querySelector('.swiper').swiper;
+const swiper = new Swiper('.swiper', {
+  speed: 500,
+  spaceBetween: 100,
+});
+// Now you can use all slider methods like
+swiper.slideNext();
+const resolution = window.innerWidth;
+// console.log(`your resolution is: ${resolution}`);
+const firstSlide = document.getElementById('one');
+const secondSlide = document.getElementById('two');
+const thirdSlide = document.getElementById('three');
+
+
+const leftBtn = document.querySelector('.left-button');
+
+const rightBtn = document.querySelector('.right-button');
