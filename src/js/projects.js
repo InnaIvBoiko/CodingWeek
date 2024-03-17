32fe17526fb21f321aa848da64d796f1543800d6
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-16 23:09:22 -0500

    final version

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,11 +1,8 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-const previosBtnIcon = document.querySelector('.left-button-icon');
+
 const previosBtn = document.querySelector('.left-button');
 const nextBtn = document.querySelector('.right-button');
-const previosActive = document.querySelector('.swiper-slide-prev');
-const nextActive = document.querySelector('.swiper-slide-next')
-const currentActive = document.querySelector('.swiper-slide-active')
 const swiper = new Swiper('.swiper', {
     spaceBetween: 100,
     navigation: true,
@@ -17,14 +14,10 @@ const swiper = new Swiper('.swiper', {
     nested: true,
 
 });
-
-const bliaColor = document.querySelector('.blia-color');
-bliaColor.style.stroke = 'red'
-  
 previosBtn.disabled = true;
-previosBtn.style.color = '#242527';
-previosBtn.style.border = '#242527';
-previosBtnIcon.style.stroke = '#242527';
+
+
+
 
 previosBtn.addEventListener('click', prev); 
 nextBtn.addEventListener('click', next);
@@ -34,25 +27,24 @@ function prev() {
 };
 function next() {
     swiper.slideNext()
-    
-}
+};
 
-        
-
-
-    swiper.on('slideChange', function () {
+swiper.on('slideChange', function () {
    
     if (swiper.isBeginning) {
         previosBtn.disabled = true;
-        previosBtn.style.backgroundColor = '#242527';
-        previosBtnIcon.style.stroke = '#242527';
-    } else if (swiper.isEnd) {
-        nextBtn.disabled = true;
+        previosBtn.style.borderColor ='#fafafa33';
+    } else {
         previosBtn.disabled = false;
+        previosBtn.style.borderColor ='#fafafa80';
+        
     }
     
-     else {
-        nextBtn.disabled = false;
+    if (swiper.isEnd) {
+        nextBtn.disabled = true;
+        nextBtn.style.borderColor = '#fafafa33';
+    } else {
+      nextBtn.disabled = false;
+      nextBtn.style.borderColor ='#fafafa80';
     }
-    })
-
+});
