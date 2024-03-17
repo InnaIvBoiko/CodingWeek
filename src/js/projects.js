857168426f6894cba5f7cde9dc91e18cef057bbe
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-16 22:21:40 -0500

    add action

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,20 +1,58 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-
-// const swiper = document.querySelector('.swiper').swiper;
+const previosBtnIcon = document.querySelector('.left-button-icon');
+const previosBtn = document.querySelector('.left-button');
+const nextBtn = document.querySelector('.right-button');
+const previosActive = document.querySelector('.swiper-slide-prev');
+const nextActive = document.querySelector('.swiper-slide-next')
+const currentActive = document.querySelector('.swiper-slide-active')
 const swiper = new Swiper('.swiper', {
-  speed: 500,
-  spaceBetween: 100,
+    spaceBetween: 100,
+    navigation: true,
+    a11y: true,
+    allowTouchMove: true,
+    centeredSlides: true,
+    keyboard: true,
+    mousewheel: true,
+    nested: true,
+
 });
-// Now you can use all slider methods like
-swiper.slideNext();
-const resolution = window.innerWidth;
-// console.log(`your resolution is: ${resolution}`);
-const firstSlide = document.getElementById('one');
-const secondSlide = document.getElementById('two');
-const thirdSlide = document.getElementById('three');
+
+const bliaColor = document.querySelector('.blia-color');
+bliaColor.style.stroke = 'red'
+  
+previosBtn.disabled = true;
+previosBtn.style.color = '#242527';
+previosBtn.style.border = '#242527';
+previosBtnIcon.style.stroke = '#242527';
+
+previosBtn.addEventListener('click', prev); 
+nextBtn.addEventListener('click', next);
+
+function prev() {
+    swiper.slidePrev()
+};
+function next() {
+    swiper.slideNext()
+    
+}
+
+        
 
 
-const leftBtn = document.querySelector('.left-button');
+    swiper.on('slideChange', function () {
+   
+    if (swiper.isBeginning) {
+        previosBtn.disabled = true;
+        previosBtn.style.backgroundColor = '#242527';
+        previosBtnIcon.style.stroke = '#242527';
+    } else if (swiper.isEnd) {
+        nextBtn.disabled = true;
+        previosBtn.disabled = false;
+    }
+    
+     else {
+        nextBtn.disabled = false;
+    }
+    })
 
-const rightBtn = document.querySelector('.right-button');
