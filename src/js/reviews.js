b195c1179e72025886ebe02d7743e2729f7d710d
MilaIlchenko <147260557+MilaIlchenko@users.noreply.github.com>
2024-03-18 11:29:54 +0100

    add

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,2 +1,47 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+
+const previosBtn = document.querySelector('.left-button');
+const nextBtn = document.querySelector('.right-button');
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 100,
+    navigation: true,
+    a11y: true,
+    allowTouchMove: true,
+    centeredSlides: true,
+    keyboard: true,
+    mousewheel: true,
+    nested: true,
+
+});
+previosBtn.disabled = true;
+
+previosBtn.addEventListener('click', prev); 
+nextBtn.addEventListener('click', next);
+
+function prev() {
+    swiper.slidePrev()
+};
+function next() {
+    swiper.slideNext()
+};
+
+swiper.on('slideChange', function () {
+   
+    if (swiper.isBeginning) {
+        previosBtn.disabled = true;
+        previosBtn.style.borderColor ='#fafafa33';
+    } else {
+        previosBtn.disabled = false;
+        previosBtn.style.borderColor ='#fafafa80';
+        
+    }
+    
+    if (swiper.isEnd) {
+        nextBtn.disabled = true;
+        nextBtn.style.borderColor = '#fafafa33';
+    } else {
+      nextBtn.disabled = false;
+      nextBtn.style.borderColor ='#fafafa80';
+    }
+});
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import about from './js/about';
 import FAQ from './js/faq';
 // import hero from "./js/hero";
 import './js/projects';
+import './js/reviews';
 // import header from "./js/header";
 // import footer from "./js/footer";
 // import reviews from "./js/reviews";
