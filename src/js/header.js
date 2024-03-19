17a0817ae433dc51d4ebaecaa94a790a4bc4b377
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-19 01:15:13 -0500

    burger menu HTML & CSS & start JS

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,23 @@
 const menu = document.querySelector(".header-menu-btn");
-menu.addEventListener('click', openClose => {
-    
+const toggle = document.querySelector(".modal-header")
+const menuBurger = document.querySelector(".header-burger-btn");
 
-
+menu.addEventListener("click", () => {
+    console.log("check");
+    if (toggle.classList.contains("is-open")) {
+        toggle.classList.remove("is-open");
+    }
+    else {
+        toggle.classList.add("is-open")
+    }
 });
 
+menuBurger.addEventListener("click", () => {
+    
+})
+
+
+
+
+  
+  
