f3d8f047f4734d7aa9f8d64b74b91a973c7bffbe
InnaIvBoiko <inna_boiko@libero.it>
2024-03-20 22:20:28 +0100

    Section about swiper for skills

--- src/js/about.js
+++ src/js/about.js
@@ -1,13 +1,12 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 const  aboutAccordion = new Accordion('.about-accordеon-container', {
   showMultiple: true,
 });
 aboutAccordion.open(0);
-
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
 
 // Function to toggle section visibility
 function toggleSection(id) {
@@ -21,14 +20,24 @@ function toggleSection(id) {
 
 // Swiper configuration
 const swiper = new Swiper('.swiper-about', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  keyboard: {
-    enabled: true,
-  },
+  slidesPerView: 2,
+  spaceBetween: 0,
+  a11y: true,
+  allowTouchMove: true,
+  keyboard: true,
+  mousewheel: true,
+  nested: true,
+  centeredSlides: false,
+  loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
-    // prevEl: '.swiper-button-prev',
   },
-  loop: true,
+  breakpoints: {
+        768: {
+            slidesPerView: 3, 
+        },
+        1440: {
+          slidesPerView: 6, 
+        }
+    }
 });
