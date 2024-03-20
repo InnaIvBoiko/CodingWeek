3d0b00773f7f6593577497b80b0f113cf03e1d67
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-03-20 19:56:37 +0200

    new

--- src/js/about.js
+++ src/js/about.js
@@ -1,76 +1,34 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-// ================ACCORDION=================
-
-const aboutAccordion = new Accordion('.accordion-container', {
+const aboutAccordion = new Accordion('.about-accordеon-container', {
   showMultiple: true,
 });
+aboutAccordion.open(0);
 
-export default aboutAccordion;
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-// ================SWIPER=================
+// Function to toggle section visibility
+function toggleSection(id) {
+  const section = document.getElementById(id);
+  if (section.style.display === 'none') {
+    section.style.display = 'block';
+  } else {
+    section.style.display = 'none';
+  }
+}
 
-const swiperAbout = new Swiper('.swiper-about', {
-  loop: true,
-  speed: 750,
-
-  navigation: {
-    nextEl: '.about-me-skills-button',
-  },
-
-  grabCursor: true,
-  simulateTouch: true,
+// Swiper configuration
+const swiper = new Swiper('.swiper-about', {
+  slidesPerView: 3,
+  spaceBetween: 30,
   keyboard: {
     enabled: true,
   },
-
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 0,
-    },
-    // when window width is >= 678px
-    678: {
-      slidesPerView: 3,
-    },
-    // when window width is >= 1440px
-    1440: {
-      slidesPerView: 6,
-    },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
   },
-
-  on: {
-    init: function () {
-      const activeIndex = this.activeIndex;
-      this.slides.forEach((slide, index) => {
-        if (index === activeIndex) {
-          slide.classList.add('active-slide');
-        } else {
-          slide.classList.remove('active-slide');
-        }
-      });
-    },
-    slideChange: function () {
-      const activeIndex = this.activeIndex;
-      this.slides.forEach((slide, index) => {
-        if (index === activeIndex) {
-          slide.classList.add('active-slide');
-        } else {
-          slide.classList.remove('active-slide');
-        }
-      });
-    },
-  },
+  loop: true,
 });
-
-// ================ACCORDION=================
-
-const accordionAbout = new Accordion('.accordion-container-about', {
-  duration: 700,
-  showMultiple: true,
-});
-accordionAbout.open(0);
