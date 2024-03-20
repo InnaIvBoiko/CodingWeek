b6deb870121a3e3abe925c3671623d00b0e74a27
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2024-03-20 06:20:29 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,4 @@ import './js/faq';
 import './js/covers';
 import './js/reviews';
 import './js/footer';
-// import header from "./js/header";
-// import "./js/reviews";
-// libraries
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
-// import Swiper from 'swiper';
-// import 'swiper/css';
+
