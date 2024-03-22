d0c300f83940d52a70039f5e5e8ae8f67dfc21c8
InnaIvBoiko <inna_boiko@libero.it>
2024-03-22 10:46:17 +0100

    add scrollto for all button

--- src/main.js
+++ src/main.js
@@ -5,3 +5,25 @@ import './js/faq';
 import './js/covers';
 import './js/reviews';
 import './js/footer';
+
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.scrollto').offsetHeight;
+
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
