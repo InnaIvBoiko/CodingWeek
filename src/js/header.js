9a9b0574b3a65b967ea7339b3fc67292ead81385
InnaIvBoiko <inna_boiko@libero.it>
2024-03-22 14:04:29 +0100

    Fixing modal windows

--- src/js/header.js
+++ src/js/header.js
@@ -1,57 +1,48 @@
-const menu = document.querySelector(".header-menu-btn");
-const toggle = document.querySelector(".modal-header")
-const menuBurger = document.querySelector(".header-burger-btn");
-const closeBtn = document.querySelector(".modal-header-body-close-btn");
-const menuCloseLink = document.querySelector(".item-link-menu");
-const aboutLink = document.querySelector(".item-link-about");
-const benefitsLink = document.querySelector(".item-link-benefits");
-const projectsLink = document.querySelector(".item-link-projects");
-const faqLink = document.querySelector(".item-link-faq");
-const orderLink = document.querySelector(".modal-order-btn-link");   
-closeBtn.addEventListener("click", closeModal)
- 
-menu.addEventListener("click", () => {
-    toggle.classList.add("is-open")
-    });
+const menuBtn = document.querySelector('.header-menu-btn');
+const openMenu = document.querySelector('.header-menu-list');
 
-menuBurger.addEventListener("click", () => {
-    toggle.classList.add("is-open");
-    });
+const menuBurger = document.querySelector('.header-burger-btn');
+const openBurger = document.querySelector('.modal-header');
+const closeBurger = document.querySelector('.modal-header-body-close-btn');
+const aboutLink = document.querySelector('.item-link-about');
+const benefitsLink = document.querySelector('.item-link-benefits');
+const projectsLink = document.querySelector('.item-link-projects');
+const faqLink = document.querySelector('.item-link-faq');
+const orderLink = document.querySelector('.modal-order-btn-link');
 
-menuCloseLink.addEventListener("click", closeModal)
-
-  function closeModal (){
-            toggle.classList.remove("is-open");
-};
-
-aboutLink.addEventListener("click", () => {
-    closeModal();
-    const about = document.getElementById("about");
-    about.scrollIntoView({ behavior: 'smooth' });
+menuBtn.addEventListener('click', () => {
+    if (openMenu.classList.contains('is-visibly-menu')) {
+        openMenu.classList.remove('is-visibly-menu');
+    } else {
+        openMenu.classList.add('is-visibly-menu');
+    };
 });
 
-
-benefitsLink.addEventListener("click", () => {
-    closeModal();
-    const benefits = document.getElementById("benefits");
-    benefits.scrollIntoView({ behavior: 'smooth' });
+menuBurger.addEventListener('click', () => {
+    openBurger.classList.add('is-open');
 });
 
-projectsLink.addEventListener("click", () => {
-    closeModal();
-     const projects = document.getElementById("projects");
-    projects.scrollIntoView({ behavior: 'smooth' });
+closeBurger.addEventListener('click', () => {
+    openBurger.classList.remove('is-open');
 });
 
-faqLink.addEventListener("click", () => {
-    closeModal();
-    const faq = document.getElementById("faq");
-    faq.scrollIntoView({ behavior: 'smooth' });
+aboutLink.addEventListener('click', () => {
+    openBurger.classList.remove('is-open');
 });
 
-orderLink.addEventListener("click", () => {
-    closeModal();
-    const footer = document.getElementById("footer");
-    footer.scrollIntoView({ behavior: 'smooth' });
-    
-})
+benefitsLink.addEventListener('click', () => {
+    openBurger.classList.remove('is-open');
+});
+
+projectsLink.addEventListener('click', () => {
+    openBurger.classList.remove('is-open');
+});
+
+faqLink.addEventListener('click', () => {
+    openBurger.classList.remove('is-open');
+});
+
+orderLink.addEventListener('click', () => {
+    openBurger.classList.remove('is-open');
+});
+
