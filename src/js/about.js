b2e078aa3692ebf932c1cb54d34766fb2733dc1d
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-03-22 17:30:46 +0200

    new

--- src/js/about.js
+++ src/js/about.js
@@ -1,34 +1,33 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-const aboutAccordion = new Accordion('.about-accordеon-container', {
+const  aboutAccordion = new Accordion('.about-accordeon-container', {
   showMultiple: true,
 });
 aboutAccordion.open(0);
 
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
-// Function to toggle section visibility
-function toggleSection(id) {
-  const section = document.getElementById(id);
-  if (section.style.display === 'none') {
-    section.style.display = 'block';
-  } else {
-    section.style.display = 'none';
-  }
-}
-
 // Swiper configuration
 const swiper = new Swiper('.swiper-about', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  keyboard: {
-    enabled: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    // prevEl: '.swiper-button-prev',
-  },
+  slidesPerView: 2,
+  spaceBetween: 0,
+  a11y: true,
+  allowTouchMove: true,
+  keyboard: true,
+  mousewheel: true,
+  nested: true,
+  centeredSlides: false,
   loop: true,
+  navigation: {
+     nextEl: '.swiper-button-next',
+  },
+  breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1440: {
+          slidesPerView: 6,
+        }
+    }
 });
