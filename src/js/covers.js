c48c3bd7336c377b2336c28935c6ec2aa8517e94
InnaIvBoiko <inna_boiko@libero.it>
2024-03-25 00:04:41 +0100

    animation for covers, fix js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,21 +1,22 @@
-// const coversWrap = document.querySelector('.marquee');
+const coversWrap = document.querySelector('#covers');
+const coversElements = document.querySelector('.marquee');
 
-// const forExport = window.addEventListener('scroll', () => {
-//   const result = isInViewport(coversWrap);
-//   if (result) {
-//     coversWrap.animate([{ transform: 'translateX(-5%)' },
-//       { transform: 'translateX(-35%)' }],
-//       { easing: 'ease-in-out', duration: 5000, iterations: 'Infinity' });
-//   };
-// });
+window.addEventListener('scroll', () => {
+    const result = isInViewport(coversWrap);
+    if (result) {
+        coversElements.animate([{ transform: 'translateX(-5%)' },
+        { transform: 'translateX(-35%)' }],
+        { easing: 'ease-in-out', duration: 5000, iterations: 'Infinity' });
+    };
+});
 
-// function isInViewport(element) {
-//   const rect = element.getBoundingClientRect();
-//   const html = document.documentElement;
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <= (window.innerHeight || html.clientHeight) &&
-//     rect.right <= (window.innerWidth || html.clientWidth)
-//   );
-// };
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const html = document.documentElement;
+    return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+};
