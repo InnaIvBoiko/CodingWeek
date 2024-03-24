9c15d57b27837e2e8eede3b59b7ddeb9259be140
InnaIvBoiko <inna_boiko@libero.it>
2024-03-24 23:27:41 +0100

    post-presentation bug fixes

--- src/js/header.js
+++ src/js/header.js
@@ -4,10 +4,7 @@ const openMenu = document.querySelector('.header-menu-list');
 const menuBurger = document.querySelector('.header-burger-btn');
 const openBurger = document.querySelector('.modal-header');
 const closeBurger = document.querySelector('.modal-header-body-close-btn');
-const aboutLink = document.querySelector('.item-link-about');
-const benefitsLink = document.querySelector('.item-link-benefits');
-const projectsLink = document.querySelector('.item-link-projects');
-const faqLink = document.querySelector('.item-link-faq');
+const burgerLinksList = document.querySelector('.modal-header-menu-list') 
 const orderLink = document.querySelector('.modal-order-btn-link');
 
 menuBtn.addEventListener('click', () => {
@@ -30,19 +27,7 @@ closeBurger.addEventListener('click', () => {
     openBurger.classList.remove('is-open');
 });
 
-aboutLink.addEventListener('click', () => {
-    openBurger.classList.remove('is-open');
-});
-
-benefitsLink.addEventListener('click', () => {
-    openBurger.classList.remove('is-open');
-});
-
-projectsLink.addEventListener('click', () => {
-    openBurger.classList.remove('is-open');
-});
-
-faqLink.addEventListener('click', () => {
+burgerLinksList.addEventListener('click', () => {
     openBurger.classList.remove('is-open');
 });
 
